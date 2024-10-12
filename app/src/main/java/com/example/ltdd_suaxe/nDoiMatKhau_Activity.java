8e5fbb8375cfe0a7d07abcf7481a25b1f9a161b4
PhanMinhNhat0601 <nhatphan612004@gmail.com>
2024-10-12 09:21:03 +0700

    update giaodien

--- app/src/main/java/com/example/ltdd_suaxe/nDoiMatKhau_Activity.java
+++ app/src/main/java/com/example/ltdd_suaxe/nDoiMatKhau_Activity.java
@@ -1,4 +1,4 @@
-package com.example.ltdd_suaxe.fragment_user;
+package com.example.ltdd_suaxe;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.ltdd_suaxe.R;
 
 public class nDoiMatKhau_Activity extends AppCompatActivity {
 
