f07abbf7056fdb796d40a8fcc7dfbb9f98d036d4
Maiz07042004 <dangmai19112016@gmail.com>
2024-10-13 12:21:37 +0700

    Update sự kiện click cửa hàng đã lưu

--- app/src/main/java/com/example/ltdd_suaxe/fragment_user/SettingsFragmentUser.java
+++ app/src/main/java/com/example/ltdd_suaxe/fragment_user/SettingsFragmentUser.java
@@ -1,15 +1,19 @@
 package com.example.ltdd_suaxe.fragment_user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.ltdd_suaxe.R;
+import com.example.ltdd_suaxe.User_CuaHang_Activity;
+import com.example.ltdd_suaxe.nCuaHangDaLuu_Activity;
 
 public class SettingsFragmentUser extends Fragment {
     private View mView;
@@ -17,6 +21,15 @@ public class SettingsFragmentUser extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_settings_user,container,false);
+        RelativeLayout cuahangdaluu = mView.findViewById(R.id.cuahangdaluu);
+
+        cuahangdaluu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), nCuaHangDaLuu_Activity.class);
+                startActivity(intent);
+            }
+        });
         return mView;
     }
 }
